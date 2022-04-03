bac3d89a44e3ab3134aee2806d4cf74a3497a4f3
rsrijana <sreejanarai@outlook.com>
2022-04-03 18:29:22 +0545

    second commit

--- app/src/main/java/com/example/journeyjournals/database/JourneyDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.journeyjournals.database;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {UserDetails.class} ,version =1)
-public abstract class JourneyDatabase extends RoomDatabase {
-    public abstract UserDAO getDao();
-}
